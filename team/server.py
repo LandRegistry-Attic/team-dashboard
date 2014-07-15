7a44150880965bc59cd3b18f9a0785b6a5de0cf6
psd <psd@whatfettle.com>
2014-07-15 09:40:56 +0100

    Render whereabouts by date from URL

--- team/server.py
+++ team/server.py
@@ -1,5 +1,6 @@
 from flask import render_template, send_file
 
+import time
 from team import app
 from members import Members
 from whereabouts import Whereabouts
@@ -16,9 +17,14 @@ def _cards():
     return render_template('cards.html', members=team.members)
 
 @app.route('/whereabouts')
-def _whereabouts():
-    places = whereabouts.places('2014-07-11')
-    return render_template('whereabouts.html', members=team.members, places=places)
+def _whereabouts_today():
+    today = time.strftime("%Y-%m-%d")
+    return _whereabouts(today)
+
+@app.route('/whereabouts/<date>')
+def _whereabouts(date):
+    places = whereabouts.places(date)
+    return render_template('whereabouts.html', date=date, members=team.members, places=places)
 
 @app.route('/photos/<path>')
 def _photo(path):
