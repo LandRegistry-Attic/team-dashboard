b81a38241a5a110da0e45fea3597ac967ea87b4b
psd <psd@whatfettle.com>
2014-07-16 21:58:56 +0100

    Default config values from the environment

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import os
 
 class Config(object):
     DEBUG = False
+    TEAM_TSV_URL = os.getenv("TEAM_TSV_URL", "team/data/team.tsv")
+    WHEREABOUTS_TSV_URL = os.getenv("WHEREABOUTS_TSV_URL", "team/data/whereabouts.tsv")
 
 class HerokuConfig(Config):
+    DEBUG = False
     pass
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    TEAM_TSV_URL = "team/data/team.tsv"
-    WHEREABOUTS_TSV_URL = "team/data/whereabouts.tsv"
-    PHOTOS_DIR = "team/data/photos"
 
 class TestConfig(Config):
     DEBUG = True
